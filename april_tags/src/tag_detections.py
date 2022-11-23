91dc8ee02db878317222df09681180555e83b62d
sriram-0311 <ramesh.anu@northeastern.edu>
2022-11-22 19:26:35 -0500

    added correct camera params

--- april_tags/src/tag_detections.py
+++ april_tags/src/tag_detections.py
@@ -20,6 +20,8 @@ class TagDetection:
 
     def callback(self, data):
         image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        image = cv2.rotate(image,cv2.ROTATE_90_CLOCKWISE)
+        image = cv2.rotate(image,cv2.ROTATE_90_CLOCKWISE)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         options = apriltag.DetectorOptions(families="tag36h11")
         detector = apriltag.Detector(options)
@@ -27,10 +29,18 @@ class TagDetection:
         for i in results:
             # Get the pose of the tag
 
-            pose, e0, e1 = detector.detection_pose(i, camera_params=(499.11014636, 498.6075723, 316.14098243, 247.3739291), tag_size=0.06)
+            pose, e0, e1 = detector.detection_pose(i, camera_params=(499.11014636, 498.6075723, 316.14098243, 247.3739291), tag_size=0.171)
             # Draw the bounding box
             cv2.rectangle(image, (int(i.corners[0][0]), int(i.corners[0][1])),(int(i.corners[2][0]),int(i.corners[2][1])), (255, 255, 0), 10)
             # Get roll pitch yaw from rotation matrix
+            r = tf.transformations.euler_from_matrix(pose)
+            # print shape of the transformation matrix
+            print(pose.shape)
+            print(r)
+            print(pose)
+            # Broadcast the transform
+            dist_from_leo = pose[2][3]*100
+            cv2.putText(image, "Distance from Leo: " + str(dist_from_leo), (int(i.corners[0][0]), int(i.corners[0][1]-20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             r = pose[0:3, 0:3]
             roll = np.arctan2(r[2, 1], r[2, 2])
             pitch = np.arctan2(-r[2, 0], np.sqrt(r[2, 1] ** 2 + r[2, 2] ** 2))
@@ -40,7 +50,10 @@ class TagDetection:
             # Publish the transformation
             self.tf_broadcaster.sendTransform(pose[0:3, 3], q, data.header.stamp, "tag_" + str(i.tag_id), "map")
         cv2.imshow("Image window", image)
-        cv2.waitKey(3)
+        #close window if q is pressed
+        cv2.waitKey(1)
+
+
 
 if __name__ == '__main__':
     rospy.init_node('tag_detection', anonymous=True)
