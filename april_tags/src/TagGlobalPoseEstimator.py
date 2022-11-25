6eebf65806436ad9d8bc20545b1c58d0917df3c1
sriram-0311 <ramesh.anu@northeastern.edu>
2022-11-25 18:51:59 -0500

    added pose estimation in map

--- april_tags/src/TagGlobalPoseEstimator.py
+++ april_tags/src/TagGlobalPoseEstimator.py
@@ -1,46 +1,63 @@
 #! /bin/env python
-# Code to transform the tag pose to the global frame
+# Code to transform the pose of april tags to map frame
 
 import rospy
 import tf
 import numpy as np
+from rospy.numpy_msg import numpy_msg
 from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import PoseWithCovariance
-from geometry_msgs.msg import Pose
-from geometry_msgs.msg import Point
-from geometry_msgs.msg import Quaternion
-from geometry_msgs.msg import TransformStamped
-from geometry_msgs.msg import Transform
-from geometry_msgs.msg import Vector3
-from geometry_msgs.msg import Vector3Stamped
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import TwistStamped
-from geometry_msgs.msg import TwistWithCovariance
-from geometry_msgs.msg import TwistWithCovarianceStamped
-from geometry_msgs.msg import Wrench
-from geometry_msgs.msg import WrenchStamped
+from std_msgs.msg import UInt8, UInt16
+# from std_msgs.msg import String
 
 class TagGlobalPoseEstimator:
     def __init__(self):
-        self.listener = tf.TransformListener()
-        self.pose_sub = rospy.Subscriber("/tag_0", PoseStamped, self.callback)
-        self.pose_pub = rospy.Publisher("/tag_0_global", PoseStamped, queue_size=10)
+        self.tf_listener = tf.TransformListener()
+        self.tf_broadcaster = tf.TransformBroadcaster()
+        self.NumberOfTagsSubscriber = rospy.Subscriber("/NumberOfTags", numpy_msg(UInt8), self.callback)
+        self.NumberOfTags = 0
 
     def callback(self, data):
-        try:
-            # Check how many tag_* topics are published
-            topic = rospy.get_published_topics()
-            # Get the number of tags
-            num_tags = len(topic)
-            print("Number of tags: ", num_tags)
-            print(topic)
+        # convert uint8 to int
+        self.NumberOfTags = np.int(data.data)
+        print("Number of tags: " + str(self.NumberOfTags))
+        for i in range(0, int(len(self.NumberOfTags))):
+            # try:
+            # read the odom to map transform
+            # odom = self.tf_listener.waitForTransform("map", "odom", rospy.Time(), rospy.Duration(4.0))
+            # look up the transform from odom to map
+            (transOdom, rotOdom) = self.tf_listener.lookupTransform("map", "odom", rospy.Time(0))
+            (transTag , rotTag) = self.tf_listener.lookupTransform("tag_","base_link", rospy.Time(0))
+            # Print the transform
+            print((transOdom, rotOdom))
+            print((transTag , rotTag))
+            # convert the tranformation and rotation from quaternion to SE(3)
+            SE3ForOdom = np.empty((4,4), float)
+            SE3ForOdom[0:3, 0:3] = tf.transformations.quaternion_matrix(rotOdom)[0:3, 0:3]
+            SE3ForOdom[0:3, 3] = transOdom
 
-        except:
-            print("No tags detected")
-        
+            SE3ForTag = np.empty((4,4), float)
+            SE3ForTag[0:3, 0:3] = tf.transformations.quaternion_matrix(rotTag)[0:3, 0:3]
+            SE3ForTag[0:3, 3] = transTag
 
+            # multiply the two SE(3) matrices
+            SE3ForMap = SE3ForMap@SE3ForOdom
+
+            # convert the SE(3) matrix to a translation and rotation
+            transMap = SE3ForMap[0:3, 3]
+            rotMap = tf.transformations.quaternion_from_matrix(SE3ForMap)
+
+            # publish the transform
+            self.tf_broadcaster.sendTransform(transMap, rotMap, rospy.Time.now(), "tag", "map")
+
+            # except:
+            #     #print("No transform found")
+            #     continue
+
+# Main function
 if __name__ == '__main__':
     rospy.init_node('Estimate_Pose', anonymous=True)
-    t = TagGlobalPoseEstimator()
-    rospy.spin()
+    try:
+        TagGlobalPoseEstimator()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
--- april_tags/src/tag_detections.py
+++ april_tags/src/tag_detections.py
@@ -8,7 +8,11 @@ import cv2
 import numpy as np
 import tf
 from sensor_msgs.msg import Image
+# from sensor_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
+from rospy.numpy_msg import numpy_msg
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import UInt8, UInt16
 
 
 class TagDetection:
@@ -17,6 +21,8 @@ class TagDetection:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/camera/image", Image, self.callback)
         self.tf_broadcaster = tf.TransformBroadcaster()
+        self.tf_listener = tf.TransformListener()
+        #self.NumberOfTagsPublisher = rospy.Publisher("/NumberOfTags", numpy_msg(UInt8), queue_size=10)
 
     def callback(self, data):
         image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -26,24 +32,38 @@ class TagDetection:
         options = apriltag.DetectorOptions(families="tag36h11")
         detector = apriltag.Detector(options)
         results = detector.detect(gray)
+
         for i in results:
             # Get the pose of the tag
 
-            pose, e0, e1 = detector.detection_pose(i, camera_params=(499.11014636, 498.6075723, 316.14098243, 247.3739291), tag_size=0.171)
+            #pose, e0, e1 = detector.detection_pose(i, camera_params=(499.11014636, 498.6075723, 316.14098243, 247.3739291), tag_size=0.171)
+            pose, e0, e1 = detector.detection_pose(i, camera_params=(-3.313, -3.313, 160.5, 120.5), tag_size=0.25)
+            #print("pose of at",pose)
+            #pose = pose@[[1, 0, 0, 0], [0, 0, -1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
             # Draw the bounding box
             cv2.rectangle(image, (int(i.corners[0][0]), int(i.corners[0][1])),(int(i.corners[2][0]),int(i.corners[2][1])), (255, 255, 0), 10)
 
-            # publish the detect image to the topic /camera/image_detected
-            self.image_pub = rospy.Publisher("/camera/image_detected", Image, queue_size=10)
             # Get roll pitch yaw from rotation matrix
             r = tf.transformations.euler_from_matrix(pose)
-            # print shape of the transformation matrix
-            # print(pose.shape)
-            # print(r)
-            # print(pose)
+            (transOdom, rotOdom) = self.tf_listener.lookupTransform("odom", "base_footprint", rospy.Time(0))
+            SE3ForOdom = np.empty((4,4), float)
+            SE3ForOdom[0:3, 0:3] = tf.transformations.quaternion_matrix(rotOdom)[0:3, 0:3]
+            #print(SE3ForOdom)
+            SE3ForOdom[0:3, 3] = transOdom
+
             # Broadcast the transform
             dist_from_leo = pose[2][3]*100
             cv2.putText(image, "Distance from Leo: " + str(dist_from_leo), (int(i.corners[0][0]), int(i.corners[0][1]-20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            
+            pose_prime = SE3ForOdom@pose
+            r = pose_prime[0:3, 0:3]
+            roll = np.arctan2(r[2, 1], r[2, 2])
+            pitch = np.arctan2(-r[2, 0], np.sqrt(r[2, 1] ** 2 + r[2, 2] ** 2))
+            yaw = np.arctan2(r[1, 0], r[0, 0])
+            # Convert to quaternion
+            q = quaternion_from_euler(roll, pitch, yaw)
+            self.tf_broadcaster.sendTransform(pose_prime[0:3, 3], q, data.header.stamp, "tag1_" + str(i.tag_id), "map")
+            pose = pose@SE3ForOdom
             r = pose[0:3, 0:3]
             roll = np.arctan2(r[2, 1], r[2, 2])
             pitch = np.arctan2(-r[2, 0], np.sqrt(r[2, 1] ** 2 + r[2, 2] ** 2))
@@ -51,10 +71,13 @@ class TagDetection:
             # Convert to quaternion
             q = quaternion_from_euler(roll, pitch, yaw)
             # Publish the transformation
-            self.tf_broadcaster.sendTransform(pose[0:3, 3], q, data.header.stamp, "tag_" + str(i.tag_id), "map")
-        # cv2.imshow("Image window", image)
+            self.tf_broadcaster.sendTransform(pose[0:3, 3], q, data.header.stamp, "tag2_" + str(i.tag_id), "map")
+            # append the tag id to the array
+        # publish the number of tags found
+        #self.NumberOfTagsPublisher.publish(tagIdsFound)
+        cv2.imshow("Image window", image)
         # #close window if q is pressed
-        # cv2.waitKey(1)
+        cv2.waitKey(1)
 
 
 
